e9e2375b72c6cb7f57f375fa88b30378f0b06105
BencsikBoglárBianka <bencsikboglarbianka@ktch.hu>
2024-10-07 23:23:17 +0200

    Netwrokk

---
+++ src/App.java
@@ -0,0 +1,16 @@
+/*
+* File: App.java
+* Author: Bencsik Boglár Bianka
+* Copyright: 2024, Bencsik Boglár Bianka
+* Group: Szoft II/I/N
+* Date: 2024-10-06
+* Github: https://github.com/BencsikBoglarBianka/Rombatl
+* Licenc: GNU GPL
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        ConnectionManager manager = new ConnectionManager();
+        manager.establishConnection("Network");
+        manager.terminateConnection();
+    }
+}
---
+++ src/Network.java
@@ -0,0 +1,20 @@
+/*
+* File: Network.java
+* Author: Bencsik Boglár Bianka
+* Copyright: 2024, Bencsik Boglár Bianka
+* Group: Szoft II/I/N
+* Date: 2024-10-06
+* Github: https://github.com/BencsikBoglarBianka/Rombatl
+* Licenc: GNU GPL
+*/
+class ConnectionManager {
+    public void establishConnection(String networkName) {
+        System.out.println("Connecting to " + networkName + "...");
+        // Kapcsolódási logika
+    }
+
+    public void terminateConnection() {
+        System.out.println("Disconnecting...");
+        // Kapcsolat bontási logika
+    }
+}
